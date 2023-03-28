91394e36eb890747718078d5e4a70a62db8c179d
yunisaliosman <yunis.osman@student.moringaschool.com>
2023-03-28 15:02:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.startsWith(query));
+  }
+  
+  function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name === query);
+  }
+  
